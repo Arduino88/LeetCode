e3066e67416a56c44640bf163b981b08e5e56238
Simon <simonharrisonsiena@gmail.com>
2024-05-01 13:05:13 -0400

    bug fix

--- best-sum.py
+++ best-sum.py
@@ -1,35 +1,34 @@
 def bestSum(targetSum: int, numbers: list, memo = {}):
     if targetSum == 0:
-        print('BASE CASE')
         return []
     
-    if targetSum < 0:
-        print ('TARGET BELOW ZERO')
-        return None
-
     if targetSum in memo:
         return memo[targetSum]
-
-    shortestCombination = None
     
-    for item in numbers:
-        remainder = 0
-        if item <= targetSum:
-            remainder = targetSum - item
-            remainderCombination = bestSum(remainder, numbers)
+    
+    smallestCombination = None
+    
+    for num in numbers:
+        if num > targetSum:
+            continue
         
-            print ('RemainderCombination:', remainderCombination, 'Target:', targetSum)
-            remainderCombination.append(item)
-            combination = remainderCombination
-            print (combination)
-            if len(shortestCombination) is 0 or len(combination) < len(shortestCombination):
-                shortestCombination = combination
+        returnedList = bestSum(targetSum - num, numbers, memo)
+        
+        if returnedList == None:
+            continue
+        
+        combination = [num] + returnedList
+        
+        if smallestCombination is None:
+            smallestCombination = combination
+            continue
             
+        if len(smallestCombination) > len(combination):
+            smallestCombination = combination
+            
+    memo[targetSum] = smallestCombination
+    #print(f"Target: {targetSum}, smallest combination: {smallestCombination}")
+    return smallestCombination
 
-    memo[targetSum] = shortestCombination
-    return shortestCombination
-        
 
-
-
-print('FINAL', bestSum(50, [1, 5, 10, 25]))
+print(bestSum(3, [2, 5, 10, 25]))
